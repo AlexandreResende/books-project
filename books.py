0651bd2bf6aa30bdcfccded725124db9b4a3648d
AlexandreResende <alexandre.amres@gmail.com>
2026-07-29 17:58:44 -0300

    refactor: add book class and populate BOOKS variable

--- books.py
+++ books.py
@@ -1,8 +1,12 @@
 from fastapi import  FastAPI, Response
 
+from Entities.bookEntity import Book
+
 app = FastAPI()
 
-BOOKS = []
+BOOKS = [
+    Book(1, "Eragon", "Christopher Paolini", "Inheritance trilogy", 5),
+]
 
 @app.get("/healthz")
 async def health_check():
