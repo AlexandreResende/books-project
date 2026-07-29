a08cba1cf4f874bcdc283526e2f397932bc7bfa6
AlexandreResende <alexandre.amres@gmail.com>
2026-07-29 17:51:49 -0300

    chore: initializing new books project

--- books.py
+++ books.py
@@ -1,79 +1,10 @@
-from fastapi import FastAPI, Response, status, Body
+from fastapi import  FastAPI, Response
 
 app = FastAPI()
 
-books = [
-    { 'id': 1, 'title': 'Eragon', 'author': 'Christopher Paolini', 'category': 'fantasy'}
-]
+BOOKS = []
 
 @app.get("/healthz")
 async def health_check():
-    return { "message": "Application healthy" }
+    return { "message": "Ok" }
 
-# using category query param to filter the result on get_books endpoint
-@app.get("/books")
-async def get_books(category: str = None):
-    result = []
-
-    if category is None:
-        return { "books": books }
-
-    for book in books:
-        if book.get('category').casefold() == category.casefold():
-            result.append(book)
-
-    return { 'books': result }
-
-@app.get("/books/{book_id}")
-async def get_book_by_id(book_id: int, response: Response):
-    for b in books:
-        if b.get('id') == book_id:
-            return b
-
-    response.status_code = status.HTTP_404_NOT_FOUND
-    return { "message": "Book not found" }
-
-@app.get("/books")
-async def get_books_by_author(author: str, response: Response):
-    authors_books = []
-    for book in books:
-        if book.get("author").casefold() == author:
-            authors_books.append(book)
-
-    return { "books": authors_books }
-
-@app.post("/books")
-async def create_book(response: Response, new_book=Body()):
-    books_length = len(books)
-    books.append({ 'id': books_length + 1, **new_book })
-
-    response.status_code = status.HTTP_201_CREATED
-
-    return {}
-
-@app.put("/books/{book_id}")
-async def update_book(book_id: int, response: Response, updated_book=Body()):
-    for book in books:
-        if book.get("id") == book_id:
-            # remove occurrence of old book from list
-            books.remove(book)
-            # overwrite data from old book with new book data
-            book = { **book, **updated_book }
-            # append updated book to list
-            books.append(book)
-            return book
-
-    response.status_code = status.HTTP_404_NOT_FOUND
-    return { "message": "Book not found" }
-
-@app.delete("/books/{book_id}")
-async def delete_book(book_id: int, response: Response):
-    for book in books:
-        if book.get("id") == book_id:
-            books.remove(book)
-
-            response.status_code = status.HTTP_204_NO_CONTENT
-            return {}
-
-    response.status_code = status.HTTP_404_NOT_FOUND
-    return { "message": "Book not found"}
