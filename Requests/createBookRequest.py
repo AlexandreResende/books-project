3b485a158c6882e3ddc5c7a6428e6694bfdc5a45
AlexandreResende <alexandre.amres@gmail.com>
2026-07-31 23:09:49 -0300

    refactor: add model_config to CreateBookRequest class validator

--- Requests/createBookRequest.py
+++ Requests/createBookRequest.py
@@ -5,8 +5,19 @@ from pydantic import BaseModel, Field, ValidationError, field_validator
 class CreateBookRequest(BaseModel):
     title: str = Field(min_length=3)
     author: str = Field(min_length=3)
-    description: Optional[str] = None
+    description: Optional[str] = Field(default=None, description="Description field is optional, but if present must have less than 150 characters")
     rating: int = Field(gt=0, lt=6)
+
+    model_config = {
+        "json_schema_extra": {
+            "example": {
+                "title": "Fluent Python",
+                "author": "Luciano Ramalho",
+                "description": "Python book from zero to hero",
+                "rating": 5
+            }
+        }
+    }
 
     @field_validator('description')
     @classmethod
