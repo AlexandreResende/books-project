ba564616df9b53ce72d8f28ef2292666e1fe1806
AlexandreResende <alexandre.amres@gmail.com>
2026-08-03 17:43:56 -0300

    refactor: enhance request class

--- Requests/createBookRequest.py
+++ Requests/createBookRequest.py
@@ -7,6 +7,7 @@ class CreateBookRequest(BaseModel):
     author: str = Field(min_length=3)
     description: Optional[str] = Field(default=None, description="Description field is optional, but if present must have less than 150 characters")
     rating: int = Field(gt=0, lt=6)
+    published_year_date: Optional[int] = Field(default=None, description="The year the book was published")
 
     model_config = {
         "json_schema_extra": {
@@ -14,12 +15,13 @@ class CreateBookRequest(BaseModel):
                 "title": "Fluent Python",
                 "author": "Luciano Ramalho",
                 "description": "Python book from zero to hero",
-                "rating": 5
+                "rating": 5,
+                "published_year_date": 2010
             }
         }
     }
 
-    @field_validator('description')
+    @field_validator("description")
     @classmethod
     def validate_description(cls, description):
         if description is not None:
@@ -27,3 +29,11 @@ class CreateBookRequest(BaseModel):
             return description
 
         return None
+
+    @field_validator("published_year_date")
+    @classmethod
+    def validate_published_year_date(cls, published_year_date):
+        if published_year_date is not None:
+            assert published_year_date > 1300
+
+        return published_year_date
--- Requests/updateBookRequest.py
+++ Requests/updateBookRequest.py
@@ -7,11 +7,20 @@ class UpdateBookRequest(BaseModel):
     title: Optional[str] = Field(description="Title of the book", default=None)
     description: Optional[str] = Field(description="A brief description of the book", default=None)
     rating: Optional[int] = Field(description="The new rating of the movie", default=None)
+    published_year_date: Optional[int] = Field(description="The year the book was published", default=None)
+
+    @field_validator("published_year_date")
+    @classmethod
+    def validate_published_year_date(cls, published_year_date):
+        if published_year_date is not None:
+            assert published_year_date > 1300
+
+        return published_year_date
 
     @field_validator("author")
     @classmethod
     def validate_author(cls, author):
-        if author:
+        if author is not None:
             assert len(author) < 100
 
         return author
@@ -19,7 +28,7 @@ class UpdateBookRequest(BaseModel):
     @field_validator("title")
     @classmethod
     def validate_title(cls, title):
-        if title:
+        if title is not None:
             assert len(title) < 100
 
         return title
@@ -27,7 +36,7 @@ class UpdateBookRequest(BaseModel):
     @field_validator("description")
     @classmethod
     def validate_description(cls, description):
-        if description:
+        if description is not None:
             assert len(description) < 150
 
         return description
@@ -35,7 +44,7 @@ class UpdateBookRequest(BaseModel):
     @field_validator("rating")
     @classmethod
     def validate_rating(cls, rating):
-        if rating:
+        if rating is not None:
             assert 0 < rating < 6
 
         return rating
