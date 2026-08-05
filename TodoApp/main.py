98c814d00c2cc64d98c834945a7d845c40f59572
AlexandreResende <alexandre.amres@gmail.com>
2026-08-05 18:14:14 -0300

    feature: add endpoint to get all todos

--- TodoApp/main.py
+++ TodoApp/main.py
@@ -1,17 +1,28 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends, status
+from typing import Annotated
+from sqlalchemy.orm import Session
 
 import models
-from TodoApp.database import SessionLocal
-from database import engine
+from models import Todos
+from database import engine, SessionLocal
 
 app = FastAPI()
 
 models.Base.metadata.create_all(bind=engine)
 
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close
+
 @app.get("/healthz")
 def health_check():
     return { "message": "Ok"}
 
-@app.get("/todos")
-def get_todos():
-    pass
+@app.get("/todos", status_code=status.HTTP_200_OK)
+def get_all_todos(db: Annotated[Session, Depends(get_db)]):
+    todos = db.query(Todos).all()
+
+    return { "todos": todos }
