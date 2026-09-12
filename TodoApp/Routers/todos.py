6b2e513b7550e1ddd128643e1fab36a0307bb3be
AlexandreResende <alexandre.amres@gmail.com>
2026-09-12 19:08:15 -0300

    refactor: add router structure to project

---
+++ TodoApp/Routers/todos.py
@@ -0,0 +1,52 @@
+from fastapi import APIRouter, HTTPException, Path, status
+
+from Requests.createTodoRequest import CreateTodoRequest
+from Requests.updateTodoRequest import UpdateTodoRequest
+from Entities.todoEntity import TodoEntity
+from container import todos_repository
+
+router = APIRouter()
+
+@router.get("/healthz")
+async def health_check():
+    return { "message": "Ok"}
+
+@router.get("/todos", status_code=status.HTTP_200_OK)
+async def get_all_todos(repository: todos_repository):
+    todos = repository.get_all_todos()
+
+    return { "todos": todos }
+
+@router.get("/todos/{todo_id}", status_code=status.HTTP_200_OK)
+async def get_todo_by_id(repository: todos_repository, todo_id: int = Path(gt=0)):
+    record = repository.get_todo_by_id(todo_id)
+
+    if record is None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Todo not found")
+
+    return record
+
+@router.post("/todos", status_code=status.HTTP_201_CREATED)
+async def create_todo(repository: todos_repository, todo_request: CreateTodoRequest):
+    repository.create_todo(TodoEntity(**todo_request.model_dump()))
+
+    return {}
+
+@router.put("/todos/{todo_id}", status_code=status.HTTP_204_NO_CONTENT)
+async def update_todo(repository: todos_repository, updated_todo_request: UpdateTodoRequest, todo_id: int = Path(gt=0)):
+    todo_new_data = updated_todo_request.model_dump()
+    result = repository.update_todo(todo_id, todo_new_data)
+
+    if result is None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Todo not found")
+
+    return
+
+@router.delete("/todos/{todo_id}", status_code=status.HTTP_200_OK)
+async def delete_todo(repository: todos_repository, todo_id: int = Path(gt=0)):
+    result = repository.delete_todo(todo_id)
+
+    if result is None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Record not found")
+
+    return { "message": "Record deleted" }
--- TodoApp/main.py
+++ TodoApp/main.py
@@ -1,56 +1,12 @@
-from fastapi import FastAPI, HTTPException, Path, status
+from fastapi import FastAPI
 
 import models
 from database import engine
-from Requests.createTodoRequest import CreateTodoRequest
-from Requests.updateTodoRequest import UpdateTodoRequest
-from Entities.todoEntity import TodoEntity
-from container import todos_repository
+from Routers import auth, todos
 
 app = FastAPI()
 
 models.Base.metadata.create_all(bind=engine)
 
-@app.get("/healthz")
-async def health_check():
-    return { "message": "Ok"}
-
-@app.get("/todos", status_code=status.HTTP_200_OK)
-async def get_all_todos(repository: todos_repository):
-    todos = repository.get_all_todos()
-
-    return { "todos": todos }
-
-@app.get("/todos/{todo_id}", status_code=status.HTTP_200_OK)
-async def get_todo_by_id(repository: todos_repository, todo_id: int = Path(gt=0)):
-    record = repository.get_todo_by_id(todo_id)
-
-    if record is None:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Todo not found")
-
-    return record
-
-@app.post("/todos", status_code=status.HTTP_201_CREATED)
-async def create_todo(repository: todos_repository, todo_request: CreateTodoRequest):
-    repository.create_todo(TodoEntity(**todo_request.model_dump()))
-
-    return {}
-
-@app.put("/todos/{todo_id}", status_code=status.HTTP_204_NO_CONTENT)
-async def update_todo(repository: todos_repository, updated_todo_request: UpdateTodoRequest, todo_id: int = Path(gt=0)):
-    todo_new_data = updated_todo_request.model_dump()
-    result = repository.update_todo(todo_id, todo_new_data)
-
-    if result is None:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Todo not found")
-
-    return
-
-@app.delete("/todos/{todo_id}", status_code=status.HTTP_200_OK)
-async def delete_todo(repository: todos_repository, todo_id: int = Path(gt=0)):
-    result = repository.delete_todo(todo_id)
-
-    if result is None:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Record not found")
-
-    return { "message": "Record deleted" }
+app.include_router(auth.router)
+app.include_router(todos.router)
