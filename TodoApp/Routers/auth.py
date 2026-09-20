f8882c184cd30bcfc579bf8b0070e85e359f9e87
AlexandreResende <alexandre.amres@gmail.com>
2026-09-20 15:43:37 -0300

    refactor: generate valid jwt bearer token on login endpoint

--- TodoApp/Routers/auth.py
+++ TodoApp/Routers/auth.py
@@ -1,5 +1,8 @@
+from datetime import datetime, timedelta
+
 from fastapi import APIRouter, HTTPException, status
 from passlib.context import CryptContext
+from jose import jwt
 
 from container import users_repository
 from Requests.Auth.loginRequest import LoginRequest
@@ -7,6 +10,10 @@ from Requests.Auth.loginRequest import LoginRequest
 router = APIRouter()
 
 bcrypt_context = CryptContext(schemes=['bcrypt'], deprecated='auto')
+
+SECRET_KEY = 'de4c91674143dfc54163c0f54774809cff3e4eb7b25b838d65f67adcf66f6fe1'
+ALGORITHM = 'HS256'
+EXPIRES_IN = 60 * 60 * 24
 
 @router.get("/auth/")
 async def get_user():
@@ -25,4 +32,10 @@ async def login(repository: users_repository, request: LoginRequest):
     if bcrypt_context.verify(password, user.password):
         raise HTTPException(status.HTTP_403_FORBIDDEN, detail='Invalid username or password')
 
-    return { 'access_token': '', 'refresh_token': '' }
+    # enconding for kwt token
+    encode = { 'sub': username, 'id': user.id }
+    expires = datetime.now() + timedelta(seconds=EXPIRES_IN)
+    encode.update({'exp': expires})
+    access_token = jwt.encode(encode, SECRET_KEY, algorithm=ALGORITHM)
+
+    return { 'access_token': access_token, 'refresh_token': '' }
