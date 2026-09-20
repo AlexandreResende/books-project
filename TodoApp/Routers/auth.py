235fc7b3172717d125faa92b4da3c4788160569d
AlexandreResende <alexandre.amres@gmail.com>
2026-09-20 16:04:50 -0300

    refactor: create function to decode jwt token

--- TodoApp/Routers/auth.py
+++ TodoApp/Routers/auth.py
@@ -1,8 +1,11 @@
 from datetime import datetime, timedelta
+from typing import Annotated
 
 from fastapi import APIRouter, HTTPException, status
+from fastapi.params import Depends
+from fastapi.security import OAuth2PasswordBearer
 from passlib.context import CryptContext
-from jose import jwt
+from jose import jwt, JWTError
 
 from container import users_repository
 from Requests.Auth.loginRequest import LoginRequest
@@ -14,6 +17,24 @@ bcrypt_context = CryptContext(schemes=['bcrypt'], deprecated='auto')
 SECRET_KEY = 'de4c91674143dfc54163c0f54774809cff3e4eb7b25b838d65f67adcf66f6fe1'
 ALGORITHM = 'HS256'
 EXPIRES_IN = 60 * 60 * 24
+
+oauth2_bearer = OAuth2PasswordBearer(tokenUrl='token')
+
+async def get_current_user(token: Annotated[str, Depends(oauth2_bearer)]):
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=ALGORITHM)
+        username: str = payload.get('sub')
+        user_id: int = payload.get('id')
+
+        if username is None or user_id is None:
+            raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail='Invalid credentials')
+
+        return {
+            'username': username,
+            'id': user_id
+        }
+    except JWTError:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail='Invalid credentials')
 
 @router.get("/auth/")
 async def get_user():
@@ -27,7 +48,7 @@ async def login(repository: users_repository, request: LoginRequest):
     user = repository.get_user_by_username(username)
 
     if not user:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail='User not found')
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail='Invalid credentials')
 
     if bcrypt_context.verify(password, user.password):
         raise HTTPException(status.HTTP_403_FORBIDDEN, detail='Invalid username or password')
